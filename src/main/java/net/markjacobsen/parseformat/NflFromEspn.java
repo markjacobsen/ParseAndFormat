163d18e9b861e6aa34d701fdeda02174b138a83a
Mark <mark@markjacobsen.net>
2021-12-06 19:07:32 -0500

    Converted over to JSONPath

--- src/main/java/net/markjacobsen/parseformat/NflFromEspn.java
+++ src/main/java/net/markjacobsen/parseformat/NflFromEspn.java
@@ -16,7 +16,6 @@ import com.cffreedom.exceptions.NetworkException;
 import com.cffreedom.utils.Convert;
 import com.cffreedom.utils.DateTimeUtils;
 import com.cffreedom.utils.Format;
-import com.cffreedom.utils.JsonUtils;
 import com.cffreedom.utils.SystemUtils;
 import com.cffreedom.utils.Utils;
 import com.cffreedom.utils.file.FileUtils;
@@ -38,8 +37,7 @@ public class NflFromEspn {
 	public static void main(String[] args) {
 		//downloadCurrentWeek();
 		
-		//parseJson();
-		parse2();
+		parseJson();
 		
 		logger.info("done");
 	}
@@ -53,151 +51,65 @@ public class NflFromEspn {
 			e.printStackTrace();
 		}
 	}
-	
+		
+	/*
+	 * "season": {
+	 * 		"year": 2021
+	 * }
+	 * "week": {
+	 * 		"number": 13,
+	 * 		...
+	 * 	"events": [
+	 * 		{
+	 * 			...
+	 * 			"shortName": "TB @ ATL"
+	 * 			...
+	 * 			"competitions": [
+	 * 				{
+	 * 					...
+	 * 					"competitors": [
+	 * 						{
+	 * 							...
+	 * 							"team": {
+	 * 								"abbreviation": "ATL"
+	 * 							}
+	 * 							"score": "17"
+	 * 						}
+	 * 						{
+	 * 							...
+	 * 							"team": {
+	 * 								"abbreviation": "TB"
+	 * 							}
+	 * 							"score": "20"
+	 * 							"records": [
+	 * 								{
+	 * 									"type": "total"
+	 * 									"summary": "8-3"
+	 * 									
+	 * 								}
+	 * 							]
+	 * 						}
+	 * 					]
+	 * 				}
+	 * 			]
+	 * 			...
+	 * 			"status": {
+	 * 				"type": {
+	 * 					"shortDetail": "2:44 - 3rd"
+	 * 				}
+	 * 			}
+	 * 			"broadcasts": [
+	 * 				{
+	 * 					"market": "national"
+	 * 					"names": [
+	 * 						"CBS"
+	 * 					]
+	 * 				}
+	 * 			]
+	 * 		}
+	 * 	]
+	 */
 	private static void parseJson() {
-		try {
-			/*
-			 * "season": {
-			 * 		"year": 2021
-			 * }
-			 * "week": {
-			 * 		"number": 13,
-			 * 		...
-			 * 		"events": [
-			 * 			{
-			 * 				...
-			 * 				"shortName": "TB @ ATL"
-			 * 				...
-			 * 				"competitions": [
-			 * 					{
-			 * 						...
-			 * 						"competitors": [
-			 * 							{
-			 * 								...
-			 * 								"team": {
-			 * 									"abbreviation": "ATL"
-			 * 								}
-			 * 								"score": "17"
-			 * 							}
-			 * 							{
-			 * 								...
-			 * 								"team": {
-			 * 									"abbreviation": "TB"
-			 * 								}
-			 * 								"score": "20"
-			 * 								"records": [
-			 * 									{
-			 * 										"type": "total"
-			 * 										"summary": "8-3"
-			 * 										
-			 * 									}
-			 * 								]
-			 * 							}
-			 * 						]
-			 * 					}
-			 * 				]
-			 * 				...
-			 * 				"status": {
-			 * 					"type": {
-			 * 						"shortDetail": "2:44 - 3rd"
-			 * 					}
-			 * 				}
-			 * 				"broadcasts": [
-			 * 					{
-			 * 						"market": "national"
-			 * 						"names": [
-			 * 							"CBS"
-			 * 						]
-			 * 					}
-			 * 				]
-			 * 			}
-			 * 		]
-			 */
-			String jsonResp = FileUtils.getFileContents(jsonRespFile);
-			JSONObject json = JsonUtils.getJsonObject(jsonResp);
-			
-			JSONObject season = JsonUtils.getJsonObject(json, "season");
-			Long yearNumber = JsonUtils.getLong(season, "year");
-			
-			JSONObject jsonWeek = JsonUtils.getJsonObject(json, "week");
-			Long weekNumber = JsonUtils.getLong(jsonWeek, "number");
-			logger.debug("Week: "+weekNumber);
-			
-			String weekFile = SystemUtils.getDirWork() + "\\nfl.scores."+yearNumber+".week."+String.format("%02d", weekNumber)+".json";
-			FileUtils.copyFile(jsonRespFile, weekFile);
-			
-			JSONArray events = JsonUtils.getJsonArray(json, "events");
-			logger.debug("Games this week: "+events.size());
-			for (int x = 0; x < events.size(); x++) {
-				// START: Game
-				JSONObject event = (JSONObject)events.get(x);
-				logger.debug("Game: "+JsonUtils.getString(event, "shortName"));
-				JSONArray competitions = JsonUtils.getJsonArray(event, "competitions");
-				for (int y = 0; y < competitions.size(); y++) {
-					JSONObject competition = (JSONObject)competitions.get(y);
-					
-					JSONObject status = JsonUtils.getJsonObject(competition, "status");
-					JSONObject statusType = JsonUtils.getJsonObject(status, "type");
-					String timeLeft = JsonUtils.getString(statusType, "shortDetail");
-					
-					String broadcastOn = "";
-					JSONArray broadcasts = JsonUtils.getJsonArray(competition, "broadcasts");
-					for (int b = 0; b < broadcasts.size(); b++) {
-						JSONObject broadcast = (JSONObject)broadcasts.get(b);
-						JSONArray names = JsonUtils.getJsonArray(broadcast, "names");
-						for (int n = 0; n < names.size(); n++) {
-							String name = (String)names.get(n);
-							if (Utils.hasLength(broadcastOn)) {
-								broadcastOn += ", ";
-							}
-							broadcastOn += name;
-						}
-					}
-					
-					Calendar gmt = Convert.toCalendar(JsonUtils.getString(competition, "date"));
-					Calendar local = DateTimeUtils.gmtToLocal(gmt);
-					logger.debug("  "+Format.date(Format.DATE_HUMAN, local)+" on "+broadcastOn+"       "+timeLeft);
-										
-					JSONArray competitors = JsonUtils.getJsonArray(competition, "competitors");
-					for (int z = 0; z < competitors.size(); z++) {
-						JSONObject competitor = (JSONObject)competitors.get(z);
-						String homeAway = JsonUtils.getString(competitor, "homeAway");
-						
-						JSONObject team = JsonUtils.getJsonObject(competitor, "team");
-						String abbrev = JsonUtils.getString(team, "abbreviation");
-						String score = JsonUtils.getString(competitor, "score");
-						
-						JSONArray records = JsonUtils.getJsonArray(competitor, "records");
-						String recordOverall = "";
-						String recordLocation = "";
-						String location = "";
-						for (int r = 0; r < records.size(); r++) {
-							JSONObject record = (JSONObject)records.get(r);
-							String type = JsonUtils.getString(record, "type");
-							String summary = JsonUtils.getString(record, "summary");
-							if (type.equalsIgnoreCase("total")) {
-								recordOverall = summary+" ("+getWinPct(summary)+")";
-							} else if (type.equalsIgnoreCase("home") && homeAway.equalsIgnoreCase("home")) {
-								recordLocation = summary+" ("+getWinPct(summary)+")";
-								location = "at home";
-							} else if (type.equalsIgnoreCase("road") && homeAway.equalsIgnoreCase("away")) {
-								recordLocation = summary+" ("+getWinPct(summary)+")";
-								location = "on the road";
-							}
-						}
-						String formattedTeamScore = Format.pad("    "+abbrev+": "+score, 22, " ", true);
-						logger.debug(formattedTeamScore+recordOverall+" overall, "+recordLocation+" "+location);
-					}
-				}
-				// END: Game
-			}
-		} catch (ParseException | FileSystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private static void parse2() {
 		List<String> output = new ArrayList<>();
 		String jsonResp = FileUtils.getFileContents(jsonRespFile);
 		DocumentContext jsonContext = JsonPath.parse(jsonResp);
@@ -208,10 +120,51 @@ public class NflFromEspn {
 		output.add(events.size()+" games");
 		
 		for (int x = 0; x < events.size(); x++) {
-			output.add(jsonContext.read("events["+x+"].shortName"));
+			String game = jsonContext.read("events["+x+"].shortName");
+			output.add(game);
 			List<Object> competitions = jsonContext.read("events["+x+"].competitions");
 			for (int y = 0; y < competitions.size(); y++) {
+				List<String> broadcasts = jsonContext.read("events["+x+"].competitions["+y+"].broadcasts[0].names");
+				String broadcastOn = "";
+				for (String name : broadcasts) {
+					if (Utils.hasLength(broadcastOn)) {
+						broadcastOn += ", ";
+					}
+					broadcastOn += name;
+				}
 				
+				String timeLeft = jsonContext.read("events["+x+"].competitions["+y+"].status.type.shortDetail");
+				String gameDate = jsonContext.read("events["+x+"].competitions["+y+"].date");
+				Calendar gmt = Convert.toCalendar(gameDate);
+				Calendar local = DateTimeUtils.gmtToLocal(gmt);
+				output.add("  "+Format.date(Format.DATE_HUMAN, local)+" on "+broadcastOn+"       "+timeLeft);
+				
+				List<Object> competitors = jsonContext.read("events["+x+"].competitions["+y+"].competitors");
+				for (int z = 0; z < competitors.size(); z++) {
+					String team = jsonContext.read("events["+x+"].competitions["+y+"].competitors["+z+"].team.abbreviation");
+					String homeAway = jsonContext.read("events["+x+"].competitions["+y+"].competitors["+z+"].homeAway");
+					String score = jsonContext.read("events["+x+"].competitions["+y+"].competitors["+z+"].score");
+					
+					List<Object> records = jsonContext.read("events["+x+"].competitions["+y+"].competitors["+z+"].records");
+					String recordOverall = "";
+					String recordLocation = "";
+					String location = "";
+					for (int r = 0; r < records.size(); r++) {
+						String type = jsonContext.read("events["+x+"].competitions["+y+"].competitors["+z+"].records["+r+"].type");
+						String summary = jsonContext.read("events["+x+"].competitions["+y+"].competitors["+z+"].records["+r+"].summary");
+						if (type.equalsIgnoreCase("total")) {
+							recordOverall = summary+" ("+getWinPct(summary)+")";
+						} else if (type.equalsIgnoreCase("home") && homeAway.equalsIgnoreCase("home")) {
+							recordLocation = summary+" ("+getWinPct(summary)+")";
+							location = "at home";
+						} else if (type.equalsIgnoreCase("road") && homeAway.equalsIgnoreCase("away")) {
+							recordLocation = summary+" ("+getWinPct(summary)+")";
+							location = "on the road";
+						}
+					}
+					String formattedTeamScore = Format.pad("    "+team+": "+score, 22, " ", true);
+					output.add(formattedTeamScore+recordOverall+" overall, "+recordLocation+" "+location);
+				}
 			}
 		}
 		
